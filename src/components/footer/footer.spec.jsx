2004cab7400ad87d30b88ee600f540be52e57de4
Антон <antonvolynskii@gmail.com>
2021-01-03 00:15:52 +1000

    jest && enzyme working configuration,test

--- src/components/footer/footer.spec.jsx
+++
@@ -1,7 +0,0 @@
-const Footer =  require("./footer");
-import { shallow } from 'enzyme';
-it ("should render Footer component", () => {
-   const component = shallow(<Footer>);
-   const wrapper = component.find(".footer__wrapper");
-       expect(wrapper.length).toBe(1);
-});
