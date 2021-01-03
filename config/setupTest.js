64c5a1fbcdbf95e4fc0cebe73b705c2fe9179811
Антон <antonvolynskii@gmail.com>
2021-01-03 23:02:58 +1000

    update jest configure

--- config/setupTest.js
+++ config/setupTest.js
@@ -1,6 +1,6 @@
-import Enzyme, {shallow,render,mount} from 'enzyme'
+import Enzyme, {shallow, render, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import toJson from 'enzyme-to-json';
+import toJson from "enzyme-to-json";
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -9,7 +9,6 @@ global.render = render;
 global.mount = mount;
 global.toJson = toJson;
 
-// Fail tests on any warning
 console.error = message => {
-    throw new Error(message);
-};
+    throw new Error(message)
+}
